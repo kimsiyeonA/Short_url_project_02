c39bcdf62f11f3346ef62d63d4983ab2c7ae8b5f
kimsiyeonA <kakasska1212@gmail.com>
2025-02-13 16:35:56 +0900

    first commit

---
+++ src/main/java/com/koreait/short_url_project_02/HomeController.java
@@ -0,0 +1,28 @@
+package com.koreait.short_url_project_02;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @Value("${custom.site.name}")
+    private String customSiteName;
+
+    @Value("${custom.site.name}")
+    private String secretKey;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Main Page: "+customSiteName;
+    }
+
+    @GetMapping("/secretKey")
+    @ResponseBody
+    public String secretKey() {
+        return "secretKey: "+secretKey;
+    }
+}
---
+++ src/test/java/com/koreait/short_url_project_02/ShortUrlProject02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.short_url_project_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlProject02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
